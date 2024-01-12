9910811a220832d48099423e7ce8db35e9477173
sedwinj <s3782699@student.rmit.edu.au>
2024-01-12 15:26:21 +1100

    Create Spring backend

---
+++ backend/src/main/java/net/sedwinj/wishlister/WishlisterApplication.java
@@ -0,0 +1,13 @@
+package net.sedwinj.wishlister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlisterApplication.class, args);
+	}
+
+}
